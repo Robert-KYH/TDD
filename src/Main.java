5f0e09f89c0d3f6db1378f7ebb80257023f8d8a4
Robert <Robert.Leffmann@student.kyh.se>
2021-10-07 11:35:18 +0200

    Skapa repo

---
+++ src/Main.java
@@ -0,0 +1,31 @@
+
+import java.util.Scanner;
+
+
+class Main {
+
+  public static void main(String[] args) {
+    print("* Minst 10 tecken");
+    print("* Endast bokstäverna A-Z, siffrorna 0-9, samt - + _ är tillåtna");
+    print("* Minst en versal, en gemen, och en siffra\n");
+
+    String lösen;
+    while (true) {
+      System.out.print("Välj ett lösenord: ");
+      lösen = new Scanner(System.in).nextLine();
+      if (verifieraLösen(lösen))  break;
+      else                        print("!!! Lösenordet är ej giltigt");
+    }
+
+    print(lösen+" - OK");
+  }
+
+
+  static boolean verifieraLösen(String s) {
+    return false;
+  }
+
+
+  static void print(String s)  {  System.out.println(s);  }
+
+}
